59b5dfea4b4c75a394c6c0b9bd905802f13a6e4f
Gush <artemij.dp@gmail.com>
2026-08-11 14:54:32 +0300

    Добавьте файлы проекта.

---
+++ MySecontMAUIApp/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace MySecondMAUIApp
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ MySecontMAUIApp/MainPage.xaml.cs
@@ -0,0 +1,16 @@
+﻿namespace MySecondMAUIApp
+{
+
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        private void Button_Clicked(object sender, System.EventArgs e)
+        {
+            string xaml = "<Label Text=\".NET MAUI\" FontSize=\"24\" />";
+            header.LoadFromXaml(xaml);
+        }
+    }
+}
